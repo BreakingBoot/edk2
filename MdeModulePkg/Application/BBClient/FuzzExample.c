b28c7dceca12e82fd31a0a92d71434e3e3fc8687
cglosner <cglosner@gmail.com>
2023-04-02 16:23:41 -0400

    Added more Debug statements

--- MdeModulePkg/Application/BBClient/FuzzExample.c
+++ MdeModulePkg/Application/BBClient/FuzzExample.c
@@ -36,12 +36,15 @@ FuzzExample1(
             UINTN src;
             ReadInput(Input, 8, &tmp);
             length = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "WRITEDATAWRAPPER: Length = %d\n", length));
             tmp = 0;
             ReadInput(Input, length, &tmp);
             src = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "WRITEDATAWRAPPER: src = 0x%p\n", src));
             tmp = 0;
             ReadInput(Input, 8, &tmp);
             offset = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "WRITEDATAWRAPPER: Offset = %d\n", offset));
             Status = Fuzz_Lockbox_WriteData_Wrapper(NULL, offset, (VOID *)&src, length);
             break;
         case WRITEDATA: 
@@ -51,13 +54,16 @@ FuzzExample1(
             UINTN dest_write;
             ReadInput(Input, 8, &tmp);
             size = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "WRITEDATA: size = %d\n", size));
             tmp = 0;
             ReadInput(Input, size, &src_write);
             src_write = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "WRITEDATA: src = 0x%p\n", src_write));
             tmp = 0;
             ReadInput(Input, 8, &dest_write);
             dest_write = StrDecimalToUintn((CHAR16 *)&tmp);
-            Status = Fuzz_Lockbox_WriteData(NULL, (VOID *)dest_write, &src_write, size);
+            DEBUG ((DEBUG_ERROR, "WRITEDATA: dest = 0x%p\n", dest_write));
+            Status = Fuzz_Lockbox_WriteData(NULL, (VOID *)&dest_write, (VOID *)&src_write, size);
             break;
         case READDATA:
             DEBUG ((DEBUG_ERROR, "EXAMPLE1: Entered READDATA\n"));
@@ -65,13 +71,16 @@ FuzzExample1(
             UINTN Offset;
             ReadInput(Input, 8, &tmp);
             read_value = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "READDATA: size = %d\n", read_value));
             tmp = 0;
             ReadInput(Input, 8, &tmp);
             Offset = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "READDATA: Offset = %d\n", Offset));
             CHAR8 *storage=AllocatePool(read_value);
             VOID **buffer = (void**)&storage;
             SetMemN(storage, read_value, '\0');
             Status = Fuzz_Lockbox_ReadData(NULL, buffer, Offset, read_value);
+            DEBUG ((DEBUG_ERROR, "READDATA: Buffer = %s\n", buffer));
             FreePool(storage);
             break;
         case WRITE_LOCK:
@@ -82,17 +91,21 @@ FuzzExample1(
             UINTN dest_w;
             ReadInput(Input, 8, &tmp);
             write_value = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "WRITELOCK: PIN = %d\n", write_value));
             tmp = 0;
             Fuzz_Lockbox_SetLockPin(NULL, write_value);
             
             ReadInput(Input, 8, &tmp);
             size_r = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "WRITELOCK: size = %d\n", size_r));
             tmp = 0;
             ReadInput(Input, size_r, &tmp);
             src_w = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "WRITELOCK: src = 0x%p\n", src_w));
             tmp =0;
             ReadInput(Input, 8, &tmp);
             dest_w = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "WRITELOCK: dest = 0x%p\n", dest_w));
             Status = Fuzz_Lockbox_WriteData(NULL, (VOID *)dest_w, &src_w, size_r);
             break;
         case READ_LOCK:
@@ -102,18 +115,22 @@ FuzzExample1(
             UINTN read;
             ReadInput(Input, 8, &tmp);
             read_val = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "READLOCK: PIN = %d\n", read_val));
             tmp = 0;
             Fuzz_Lockbox_SetLockPin(NULL, read_val);
             
             ReadInput(Input, 8, &tmp);
             read = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "READLOCK: size = %d\n", read));
             tmp = 0;
             ReadInput(Input, 8, &tmp);
             off = StrDecimalToUintn((CHAR16 *)&tmp);
+            DEBUG ((DEBUG_ERROR, "READLOCK: Offset = %d\n", off));
             CHAR8 *stor=AllocatePool(read);
             VOID **buff = (void**)&stor;
             SetMemN(stor, read, '\0');
             Status = Fuzz_Lockbox_ReadData(NULL, buff, off, read);
+            DEBUG ((DEBUG_ERROR, "READLOCK: buffer = %s\n", *buff));
             FreePool(stor);
             Status = EFI_ABORTED;
             break;
