48812b1a60f7337965cde08549e67623d7dbc47b
cglosner <cglosner@gmail.com>
2023-04-13 09:00:48 -0400

    Added preliminary CoreTrust service

---
+++ MdeModulePkg/Application/BBClient/FuzzTrust.c
@@ -0,0 +1,24 @@
+#include "FuzzTrust.h"
+
+EFI_STATUS
+EFIAPI
+FuzzTrust(
+    IN INPUT_BUFFER *Input
+)
+{
+    LIST_ENTRY             *Link;
+    EFI_CORE_DRIVER_ENTRY  *DriverEntry;
+    EFI_STATUS Status;
+    UINTN count = 0;
+    for (Link = mDiscoveredList.ForwardLink; Link != &mDiscoveredList; Link = Link->ForwardLink) {
+        DriverEntry = CR (Link, EFI_CORE_DRIVER_ENTRY, Link, EFI_CORE_DRIVER_ENTRY_SIGNATURE);
+        if(count == 7)
+        {
+            Status = gDS->Trust(DriverEntry->FvHandle, &DriverEntry->FileName);
+            break;
+        }
+        count++;
+    }
+
+    return Status;
+}
---
+++ MdeModulePkg/Application/BBClient/FuzzTrust.h
@@ -0,0 +1,12 @@
+#ifndef _FUZZTRUST_H_
+#define _FUZZTRUST_H_
+
+#include "BBClientUtilities.h"
+
+EFI_STATUS
+EFIAPI
+FuzzTrust(
+    IN INPUT_BUFFER *Input
+);
+
+#endif
